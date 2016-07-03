5f2bde6947257b93bc5548a6fdc8e058bdfd02ef
Vyacheslav Krachkov <vskrachkov@gmail.com>
2016-07-03 18:44:17 +0300

    signals added

--- studsite/statistic/models.py
+++ studsite/statistic/models.py
@@ -1,19 +1,59 @@
+import json
+import ast
+
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.apps import apps
+
+
+def trigger_validation(value):
+    """Raises ValidationError if value not in TRIGGERS."""
+    TRIGGERS = ('create', 0), ('update', 1), ('delete', 2)
+    if value not in (v[0] for v in TRIGGERS):
+        raise ValidationError('Incorrect value. Please choose one of \
+            the next value: {vals}'.format(vals=[v[0] for v in TRIGGERS]))
 
 
 class ModelInstancesChange(models.Model):
-    app_label = models.CharField(verbose_name=u'Приложение', max_length=50)
-    model_name = models.CharField(verbose_name=u'Модель', max_length=50)
-    change_time = models.DateTimeField(
-        verbose_name=u'Время изменения', auto_now_add=True)
-    change_type = None
 
     class Meta:
-        verbose_name = u''
-        verbose_name_plural = u''
+        verbose_name = 'ModelInstancesChange'
+        verbose_name_plural = 'ModelInstancesChanges'
+
+    app_label = models.CharField(max_length=50)
+    model_name = models.CharField(max_length=50)
+    change_time = models.DateTimeField(editable=False)
+    change_type = models.CharField(
+        validators=[trigger_validation], max_length=50)
+    pre_saved_data = models.CharField(max_length=50)
 
     def __str__(self):
-        return u''
+        return '{0}.{1}, {2}, {3}'.format(self.app_label,
+                                          self.model_name,
+                                          self.change_type,
+                                          self.change_time)
 
-    def save(self):
-        super().save()
+    def _get_model(self):
+        """Returns model ..."""
+        return apps.get_model(self.app_label, model_name=self.model_name)
+
+    def _get_pre_saved_data(self):
+        """
+        Returns model instance data that be before dataes have been changed or
+        created. Method returns dictionary or string if string not been
+        converted to dictionary. That's why control this moment.
+
+        """
+        try:
+            data = ast.literal_eval(self.pre_saved_data)
+        except SyntaxError:
+            return self.pre_saved_data
+        return data
+
+    @property
+    def model(self):
+        return self._get_model()
+
+    @property
+    def data(self):
+        return self._get_pre_saved_data()
--- studsite/statistic/signals.py
+++ studsite/statistic/signals.py
@@ -1,22 +1,63 @@
-from django.core.signals import request_started
-from django.db.models.signals import pre_save, pre_delete
-from django.dispatch import receiver
+from datetime import datetime
+
+from django.db.models.signals import post_delete, post_save, pre_save
+
+from students.models import Student, Group
+from .models import ModelInstancesChange
 
 
-@receiver(request_started)
-def my_callback(sender, **kwargs):
-    print("Request started!")
+class StudentAppModelsSignals:
+    """Recives signals from student app models."""
 
+    @staticmethod
+    def change(val):
+        if val:
+            return 'create'
+        else:
+            return 'update'
 
-@receiver(pre_save)
-def save_callback(sender, **kwargs):
-    obj = kwargs.get('instance', None)
-    print(obj._meta.app_label, obj._meta.model_name)
-    print(obj.__dict__)
+    def without_underline_befor_keys(instance):
+        dic = {}
+        for k, v in instance.__dict__.items():
+            if k.startswith('_'):
+                pass
+            else:
+                dic[k] = v
+        return dic
 
+    @classmethod
+    def pre_save_callback(cls, sender, instance, **kwargs):
+        """Saves pre instance data into database before it be changed."""
+        cls.pre_saved_data = cls.without_underline_befor_keys(instance)
 
-@receiver(pre_delete)
-def delete_callback(sender, **kwargs):
-    obj = kwargs.get('instance', None)
-    print(obj._meta.app_label, obj._meta.model_name)
-    print(obj.__dict__)
+    @classmethod
+    def post_save_callback(cls, sender, instance, created, **kwargs):
+        app_label = instance._meta.app_label
+        model_name = instance._meta.model.__name__
+        change_type = cls.change(created)
+        change_time = datetime.now()
+        ModelInstancesChange(app_label=app_label,
+                             model_name=model_name,
+                             change_type=change_type,
+                             change_time=change_time,
+                             pre_saved_data=cls.pre_saved_data).save()
+
+    @classmethod
+    def post_delete_callback(cls, sender, instance, **kwargs):
+        pre_saved_data = cls.without_underline_befor_keys(instance)
+        app_label = instance._meta.app_label
+        model_name = instance._meta.model.__name__
+        change_type = 'delete'
+        change_time = datetime.now()
+        ModelInstancesChange(app_label=app_label,
+                             model_name=model_name,
+                             change_type=change_type,
+                             change_time=change_time,
+                             pre_saved_data=pre_saved_data).save()
+
+pre_save.connect(
+    StudentAppModelsSignals.pre_save_callback, sender=Student)
+post_save.connect(
+    StudentAppModelsSignals.post_save_callback, sender=Student)
+post_delete.connect(
+    StudentAppModelsSignals.post_delete_callback, sender=Student)
